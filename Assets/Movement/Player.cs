10875e002bebfe5cb223da85afc0837424d39c91
Ali Ghahremani <alighahremani1377@gmail.com>
2025-01-23 13:06:17 +0330

    feat: player horizontal movement

--- Assets/Movement/Player.cs
+++ Assets/Movement/Player.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class Player : MonoBehaviour {
+    public float moveSpeed = 5f; // Speed of movement
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Horizontal movement
+        var moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        transform.localScale = moveInput switch {
+            // Flip the character sprite based on movement direction
+            > 0 => new Vector3(1, 1, 1),
+            < 0 => new Vector3(-1, 1, 1),
+            _ => transform.localScale
+        };
     }
+
 }
