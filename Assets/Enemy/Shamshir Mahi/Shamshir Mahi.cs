03c36cc5efdc1b2bf967ca289db008a1a20f1d6f
SepehrRG <srgir04@gmail.com>
2025-01-24 12:03:43 +0330

    Mini boss- Fish- Crab

---
+++ Assets/Enemy/Shamshir Mahi/Shamshir Mahi.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ShamshirMahi : MonoBehaviour
+{
+    [SerializeField] private short _moveSpeed;
+    [SerializeField] private short _attackSpeed;
+    // [SerializeField] private Player _player;
+    [SerializeField] private float _normalHeight;
+
+
+    private Rigidbody2D _rigidbody2D;
+    private BoxCollider2D _boxTrigger2D;
+    private SpriteRenderer _eyeSpriteRenderer;
+    private Vector2 _direction;
+    private short _health = 100;
+    private float distanceToPlayer;
+    private int _targetPointIndex;
+    private int _round;
+    private short _currentSpeed;
+
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _boxTrigger2D = GetComponent<BoxCollider2D>();
+        _eyeSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _direction = Vector2.right;
+        _targetPointIndex = 0;
+        _currentSpeed = _moveSpeed;
+    }
+    
+
+    private void FixedUpdate()
+    {
+
+        _rigidbody2D.velocity = _direction * _currentSpeed;
+
+
+
+    }
+
+    public void ChangeDirection()
+    {
+        _direction *= -1;
+        transform.eulerAngles = new Vector3(0, 180 - transform.eulerAngles.y, 0);
+        // _targetPointIndex = 1 - _targetPointIndex;
+        _round++;
+        if (_round == 3)
+        {
+            _currentSpeed = _moveSpeed;
+            StartCoroutine(HeightAnimation(_normalHeight));
+        }
+        else if (_round == 6)
+        {
+            _currentSpeed = _attackSpeed;
+            StartCoroutine(HeightAnimation(-1 * _normalHeight));
+            _round = 0;
+        }
+    }
+
+    public void Damaged(short damage)
+    {
+        _health -= damage;
+        if (_health < 1)
+        {
+            // GameManager.instance.AddScore(20)
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("message from " + gameObject + ": im in OnCollisionEnter2D");
+        if (other.gameObject.tag == "Player")
+        {
+            // damage player
+        }
+        else if (other.gameObject.tag == "Bullet")
+        {
+            Damaged(5);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") return;
+        ChangeDirection();
+        Debug.Log("message from " + gameObject + ": im in OnTriggerEnter2D");
+    }
+
+    IEnumerator HeightAnimation(float nH)
+    {
+        nH /= 20;
+        for (int i = 0; i < 20; i++)
+        {
+            transform.position += new Vector3(0, nH, 0);
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+}
